8e9d9ba282231139270c2bb7e3bdccf970e03c86
Yannis <yannisaaltoweb@me.com>
2012-04-16 13:27:38 +0300

    Locations deserialized.

--- mapping/server.rb
+++ mapping/server.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require 'haml'
+require 'json'
 
 get '/' do
+	locationFile = File.open("locationData.json", "r")
+	rawLocationData = locationFile.read
+	content_type :json
+	@jsonLocationData = JSON.load(rawLocationData)					# Get JSON data
+	@locationCount = @jsonLocationData.fetch("locations").count		# Number of locations in DB
 	haml :index
 end
+
