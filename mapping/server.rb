319a9c40c8d18f3000172e746be58cadf10a3958
Yannis <yannisaaltoweb@me.com>
2012-04-17 12:47:11 +0300

    Displaying current location and other pins on map.
    Loads JSON-stored location data and attempts to draw them on a map. I am unable to squash a very persistent and strange bug—current

--- mapping/server.rb
+++ mapping/server.rb
@@ -2,11 +2,15 @@ require 'sinatra'
 require 'haml'
 require 'json'
 
+configure do
+  mime_type :js, 'application/x-javascript'
+end
+
 get '/' do
 	locationFile = File.open("locationData.json", "r")
-	rawLocationData = locationFile.read
+	@rawLocationData = locationFile.read
 	content_type :json
-	@jsonLocationData = JSON.load(rawLocationData)			# Get JSON data
+	@jsonLocationData = JSON.load(@rawLocationData)			# Get JSON data
 	puts @jsonLocationData
 	@locationCount = @jsonLocationData["locations"].count	# Number of locations in DB
 
