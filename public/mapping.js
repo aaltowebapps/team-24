bcdbfb405cc3a88c8d0b0476787052944ec90604
Yannis <yannisaaltoweb@me.com>
2012-05-05 17:02:40 +0300

    Added infoWindows (aka "bubbles") to pins.
    Clicking on a pin now opens a "bubble" that will contain event information. The text is editable inline. We still need to determine whether this is sufficient, or whether a dialog to add/edit event information is still the way to go. My hunch is that editable bubbles should be enough.

--- public/mapping.js
+++ public/mapping.js
@@ -1,13 +1,21 @@
 var map;
+var bubble;
 
-function placeMarker (loc) {
+function placeMarker(loc) {
+    // Create pin.
     var latitude;
     var longitude;
     var markLocation = new google.maps.Marker({
-            position: loc,
-            draggable: true,
-            map: map
-        });
+        position: loc,
+        draggable: true,
+        map: map
+    });
+    
+    // Add event listener to pin to display bubble on click.
+    google.maps.event.addListener(markLocation, 'click', function() {
+		bubble.open(map, markLocation);
+    });
+
     map.setCenter(loc);
     latitude = loc.lat();  // to be saved in the new event!
     longitude = loc.lng();
@@ -17,14 +25,20 @@ $(function() {
 	var marker;
 	var mapOptions;
 	
+	// Create the info window (hereby referred to as 'bubble').
+	bubble = new google.maps.InfoWindow({
+		maxWidth: '40px',
+		content: '<div contentEditable = "true">Placeholder Text</div>'
+    });
+
 	navigator.geolocation.getCurrentPosition(function(geodata) {
 		// Get current position and set map options.
 		var currentLatLng = new google.maps.LatLng(geodata.coords.latitude, geodata.coords.longitude);
-		mapOptions = {center: currentLatLng, zoom: 14, mapTypeId: google.maps.MapTypeId.ROADMAP};				
+		mapOptions = {center: currentLatLng, zoom: 14, mapTypeId: google.maps.MapTypeId.ROADMAP};
 
 		// Initialize map with current position.
 		map = new google.maps.Map(document.getElementById("map"), mapOptions);
-		marker = new google.maps.Marker({position: currentLatLng, map: map, title: "You are here"});
+		placeMarker(currentLatLng);
 	
 		// Display pins with data from JSON database.
 		var jsonLocationData = $.parseJSON($("#jsonLoc").html());			// Parse div-stored JSON string.
@@ -33,14 +47,14 @@ $(function() {
 		google.maps.event.addListenerOnce(map, 'idle', function() {
 			for (var i = 0; i < jsonLocationData.locations.length; i++) {
 				var currentLoc = jsonLocationData.locations[i];
-				currentLatLng = new google.maps.LatLng(currentLoc.lat, currentLoc.lng);		// Create Google location object. 
-				marker = new google.maps.Marker({position: currentLatLng, map: map, title: currentLoc.locName});
+				currentLatLng = new google.maps.LatLng(currentLoc.lat, currentLoc.lng);		// Create Google location object.
+				placeMarker(currentLatLng);
 				console.log(jsonLocationData);
 			}
-        });         
+        });
 		
-		google.maps.event.addListener(map, 'click', function(event){
-            placeMarker(event.latLng);		
-		});			
+		google.maps.event.addListener(map, 'click', function(event) {
+            placeMarker(event.latLng);
+        });
 	});
 });
