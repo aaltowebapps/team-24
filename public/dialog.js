5efa24eaf3139f3469963cb836db3f6c5f2a2a26
Yannis <yannisaaltoweb@me.com>
2012-05-11 00:19:31 +0300

    Form now validates input. Changed name of app. Minor changes to a few UI strings. Updated JSON schema.

--- public/dialog.js
+++ public/dialog.js
@@ -3,6 +3,6 @@ $(document).delegate("#openEditEventDialog", "click", function() {
 		mode: 'blank',
 		headerText: 'Event details',
 		headerClose: true,
-		blankContent : "<div class='content' style='margin-left=10px;' data-role='content'><form action='/newEvent' method='post'></form><input id='title' style='max-width:260px;margin-left: 5px;width: 255px' name='title' placeholder='Event description' data-mini='true' type='text' /><label for='duration' style='margin-left:7px;padding-top:10px;'>Minutes to expiration: </label><input id='duration' style='margin-left: 5px;' max='300' min='1' type='range' data-mini='true' value='5' /><button id='postEntry' type='submit' data-mini='true'>Add</button><button id='deleteEntry' type='delete' data-theme='e' data-mini='true'>Delete</button></div>"
+		blankContent : "<div class='content' style='margin-left=10px;' data-role='content'><form action='/newEvent' method='post'><input id='title' style='max-width:260px;margin-left: 5px;width: 255px' name='title' placeholder='Event description' data-mini='true' type='text' required='required'/><label for='duration' style='margin-left: 7px;padding-top: 10px;'>Minutes to expiration: </label><input id='duration' style='margin-left: 5px;' max='300' min='1' type='range' data-mini='true' value='5' /><button id='postEntry' type='submit' data-mini='true'>Add</button><button id='deleteEntry' type='button' data-theme='e' data-mini='true'>Delete</button></form></div>"
 	});
 });
