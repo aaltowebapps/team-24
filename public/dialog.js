45f3f9a4874329715ea1bd03b210c8d6d97a3e0b
Yannis <yannisaaltoweb@me.com>
2012-05-10 17:52:44 +0300

    Polished event add/view/delete/edit dialog.
    I propose that we do not add a "starting date/time" field to the form, at least not yet. Such a function would greatly increase the complexity of our controller logic and time is *not abundant! We can think of our app as a "live events" app only.

--- public/dialog.js
+++ public/dialog.js
@@ -3,6 +3,6 @@ $(document).delegate("#openEditEventDialog", "click", function() {
 		mode: 'blank',
 		headerText: 'Event details',
 		headerClose: true,
-		blankContent : "<h1>Event details</h1></div><div class='content' data-role='content'><form action='/newEvent' method='post'></form><input id='title' name='title' placeholder='Event description' type='text' /><br /><div data-role='fieldcontain'><label for='duration'></label><input id='duration' max='300' min='5' type='range' value='5' /></div><br /><input id='title' name='latitude' placeholder='Latitude' type='text' /><br /><button id='postEntry' type='submit'>Add</button><button id='deleteEntry' type='delete'>Delete</button>"
+		blankContent : "<div class='content' style='margin-left=10px;' data-role='content'><form action='/newEvent' method='post'></form><input id='title' style='max-width:260px;margin-left: 5px;width: 255px' name='title' placeholder='Event description' data-mini='true' type='text' /><label for='duration' style='margin-left:7px;padding-top:10px;'>Minutes to expiration: </label><input id='duration' style='margin-left: 5px;' max='300' min='1' type='range' data-mini='true' value='5' /><button id='postEntry' type='submit' data-mini='true'>Add</button><button id='deleteEntry' type='delete' data-theme='e' data-mini='true'>Delete</button></div>"
 	});
 });
--- views/dialog.js
+++
@@ -1,8 +0,0 @@
-$(document).delegate("#openEditEventDialog", "click", function() {
-	$('<div>').simpledialog2({
-		mode: 'blank',
-		headerText: 'Event details',
-		headerClose: true,
-		blankContent : "<h1>Event details</h1></div><div class='content' data-role='content'><form action='/newEvent' method='post'></form><input id='title' name='title' placeholder='Event description' type='text' /><br /><div data-role='fieldcontain'><label for='duration'></label><input id='duration' max='300' min='5' type='range' value='5' /></div><br /><input id='title' name='latitude' placeholder='Latitude' type='text' /><br /><button id='postEntry' type='submit'>Add</button><button id='deleteEntry' type='delete'>Delete</button>"
-	});
-});
