00e0536a9c0319a39171152849239ced64475eea
Yannis <yannisaaltoweb@me.com>
2012-05-18 21:12:20 +0400

    Back to Backbone.js: Now the list view is bound with the model. Clicking on the list view takes you to the pin of that event on the map, centered and zoomed in.

--- public/dialog.js
+++ public/dialog.js
@@ -9,32 +9,49 @@ isDialogOpen = false;
 
 // The following function opens a modal dialog using the SimpleDialog2 jQuery plugin.
 displayDialog = function() {
+
 	$('<div>').simpledialog2({
+
 		mode: 'blank',
+
 		headerText: 'Event Details',
+
 		headerClose: true,
+
 		blankContent: "<div class='content' style='margin-left=10px;' data-role='content'><form action='/newEvent' method='post'><input id='title' style='max-width:260px;margin-left: 5px;width: 255px' name='title' placeholder='Event description' data-mini='true' type='text' required='required'/><label for='duration' style='margin-left: 7px;padding-top: 10px;'>Minutes to expiration: </label><input id='duration' style='margin-left: 5px' max='300' min='1' type='range' data-mini='true' value='5' /><button id='postEntry' type='submit' data-mini='true'>OK</button><button id='deleteEntry' type='button' data-theme='e' data-mini='true'>Delete Event</button></form></div>",
+
 		callbackClose: function() {
+			
 			// Announce to the event touchmove handler that the dialog is closed, so that scrolling can be reenabled.
 			isDialogOpen = false;
+			
 		}
+
 	});
+
 	// Announce to the event touchmove handler that the dialog is open so that scrolling is disabled.
 	isDialogOpen = true;
+
 };
 
 
 
 // Attach a click event listener to the "List view" button of the home screen.
 $(document).on("click", "#openEditEventDialog", function() {
+	
 	displayDialog();
+
 });
 
 
 
 // Create touchmove handler to disable scrolling when the dialog is open.
 $(function() {
+
 	$('body').on('touchmove', function(event){
+
 		if (isDialogOpen) event.preventDefault();
+
 	});
+
 });
--- public/mapping.js
+++ public/mapping.js
@@ -5,6 +5,7 @@ var currentLatLng;		// Google LatLng object for the current position.
 
 // The following function adds a pin to the map at the location loc.
 function placeMarker(loc, isCurrentPos) {
+
     // The isCurrentPos argument is a boolean value indicating if the new pin represents the current position.
     // We want this argument to be optional, for the sake of simplicity.
     if (typeof isCurrentPos == 'undefined') isCurrentPos = false;
@@ -12,13 +13,15 @@ function placeMarker(loc, isCurrentPos) {
     // Create pin object on map (and store it in markLocation).
     // Use default icon for new events and custom icon for the current location.
     var markLocation;
+
     if (isCurrentPos === false) {
 			markLocation = new google.maps.Marker({
 				position: loc,
 				draggable: true,
 				map: map
 			});
-    } else {
+    }
+	else {
 			markLocation = new google.maps.Marker({
 				position: loc,
 				draggable: false,	// Current position not draggable.
@@ -28,7 +31,7 @@ function placeMarker(loc, isCurrentPos) {
 			});
 	}
 
-	// Handler for updating the coordinates of the event on dragging the pin.
+	// Update the coordinates of the event after dragging the pin.
 	if (!isCurrentPos) {
 		google.maps.event.addListener(markLocation, 'dragend', function(event) {
 			// Find the model whose id matches the id of the pin.
@@ -62,14 +65,17 @@ function placeMarker(loc, isCurrentPos) {
 
 // Bind the "Locate Me" button.
 $(function() {
+
 	$("#locateMe").on("click", function() {
 		map.setCenter(currentLatLng);
 	});
+
 });
 
 
 
 $(function() {
+	
 	// Hide address bar and adjust map div dimensions according to the device at hand.
 	// This needs to be done after every device rotation (i.e. window size change).
 	function resetMapDivDimensions() {
@@ -80,14 +86,22 @@ $(function() {
 
 	// Initialize map and mapping functions.
 	navigator.geolocation.getCurrentPosition(function(geodata) {
+
 		// Create current position object, set map options.
 		currentLatLng = new google.maps.LatLng(geodata.coords.latitude, geodata.coords.longitude);
+		
 		var mapOptions = {
+
 			center: currentLatLng,
+
 			zoom: 14,
+
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
+
 			zoomControl: false,
+
 			streetViewControl: false
+
 		};
 		
 		// Create appropriately sized div for the map.
@@ -98,8 +112,10 @@ $(function() {
 		
 		// Before proceeding with mapping functions, ensure the map has been fully loaded.
 		google.maps.event.addListenerOnce(map, 'idle', function() {
+			
 			// Pin current location onto map.
 			var currentPosMarker = placeMarker(currentLatLng, true);
+			
 			// Refresh the current location every 30 sec at most.
 			navigator.geolocation.watchPosition(
 				function(geodata) {
@@ -109,13 +125,19 @@ $(function() {
 				function() {},
 				{enableHighAccuracy:true, maximumAge:30000, timeout:5000}
 			);
+
 			// Create an event by tapping on the map.
 			google.maps.event.addListener(map, 'click', function(event) {
-				// We need a unique, server-generated ID for the new event, to avoid data corruption.
+				
+				// Obtain server-generated ID for the new event, to avoid data corruption.
 				console.log('... Sent AJAX request for new ID, waiting for response ...');
+				
 				$.get('/nextid',
+
 						function(nextID) {
-							console.log('Got it! The new ID is', nextID);
+
+							console.log('Got it! The new ID is: ', nextID);
+							
 							// Create (add & save) a model in the collection (sends a POST request to server).
 							var i = events.create({
 								'title'		: 'My New Event',
@@ -124,6 +146,7 @@ $(function() {
 								'latitude'	: event.latLng.lat(),
 								'id'		: nextID
 							});
+
 							displayDialog();
 						}
 				);
@@ -137,39 +160,38 @@ $(function() {
 			// Get the Socket ID of this client (to be used in preventing broadcast of its own messages back to itself).
 			var socketID = null;
 			pusher.connection.bind('connected', function() {
+
 				socketID = pusher.connection.socket_id;
 				console.log('My socket id is: ', socketID);
+
 			});
 
 			//Fetch the latest tasks and trigger an update of the views
 			events.fetch();
 
 			// Setup the Pusher event handlers.
+			// Event-specific handlers would have been smoother and more economical with pin changes
+			// than using "events.fetch" in all cases. We use three listeners as placeholders until this happens
+			// (maybe in ver. 2).
 			channel.bind('posted', function(m) {
-				console.log("<<< 'Posted' >>>", m);
-				// events.create(m);
+
+				console.log(">>> Received pushed 'POST' event >>> #", m);
 				events.fetch();
 			});
 
 			channel.bind('put', function(i) {
-				console.log("<<< 'Put' # >>>", i);
+
+				console.log(">>> Received pushed 'PUT' event >>> #", i);
+				
 				// var e = events.where({id : i});	Unfortunately not available even though it appears in docs.
-				// for (var k = 0; k < events.models.length; k++) {
-				// 	e = events.at(k);
-				// 	if (e.get('id') == i) e.fetch();
-				// 	break;
-				// }
 				events.fetch();
 			});
 
 			channel.bind('deleted', function(i) {
-				console.log("<<< 'Deleted' # >>>", i);
+
+				console.log(">>> Received pushed 'DELETE' event >>> #", i);
+
 				// var e = events.where({id : i});	Unfortunately not available even though it appears in docs.
-				// for (var k = 0; k < events.models.length; k++) {
-				// 	e = events.at(k);
-				// 	if (e.get('id') == i) e.destroy();
-				// 	break;
-				// }
 				events.fetch();
 			});
         });
@@ -177,8 +199,10 @@ $(function() {
 		// On the mobile, the window is resized when the device is rotated.
 		// In those cases, we resize the div accordingly and trigger a resize event for the map.
 		$(window).resize(function (){
-			resetMapDivDimensions();						// Resize the div in which the map is locate.
+		
+			resetMapDivDimensions();						// Resize the div in which the map is located.
 			google.maps.event.trigger(map, 'resize');		// Resize the map to fully occupy its div.
+
 		});
 	});
 });
